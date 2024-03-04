4f969f5d4402f2dbb8ccd7687c8ccd34e9dd4e16
Melanie Tran <melaniexx02@gmail.com>
2024-03-04 15:06:53 -0700

    Update unicorn object

--- mythical-creatures/lib/unicorn.rb
+++ mythical-creatures/lib/unicorn.rb
@@ -1,9 +1,21 @@
 class Unicorn
-    attr_reader :name
-    attr_accessor :color, :eating
+    attr_reader :name, :color #attributes become methods
 
-    def initalize
+    def initialize(name, color = 'silver') #attributes required for object)
         @name = name
-    
+        @color = color
     end
+
+    def silver?
+        @color == 'silver' #is the unicorn silver? true or false
+    end
+
+    def say(x)
+        "**;* #{x} **;*"
+    end
+
 end
+
+unicorn = Unicorn.new('Billy')
+puts unicorn.name
+puts unicorn.say('MoMo is a lovebug')
